6da1f7415c9ad698ac7152deaeb4e5f9ccb13038
ocin-effendy <nikoardia134@gmail.com>
2023-01-31 16:16:40 +0700

    3d project

--- app.js
+++ app.js
@@ -33,11 +33,18 @@ document.addEventListener('mouseup', function(){
 var expertise = document.querySelector('.expertise');
 var project = document.querySelector('.project');
 var expertiseSection = document.querySelector('.container-expertise')
+var projectScrollSection = document.querySelector('.container-scroll-project')
 
 
 expertise.addEventListener('click', function(){
   window.scrollTo({
     top: expertiseSection.getBoundingClientRect().top,
+    behavior: 'smooth'
+  });
+});
+project.addEventListener('click', function(){
+  window.scrollTo({
+    top: projectScrollSection.getBoundingClientRect().top,
     behavior: 'smooth'
   });
 });
@@ -59,14 +66,78 @@ arrowRight.addEventListener("click", function () {
     contentFlutter.style.removeProperty("display");
     setTimeout(() => {
       contentFlutter.classList.remove("box--hidden");
-    }, 100);
+    }, 200);
   } else {
     contentFlutter.classList.add("box--hidden");
     contentFlutter.style.display = "none";
     contentAndroid.style.removeProperty("display");
     setTimeout(() => {
       contentAndroid.classList.remove("box--hidden");
-    }, 100);
+    }, 200);
 
   }
 });
+
+
+
+// project scroll
+const canvas = document.querySelector(".canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+const frameCount = 12;
+
+const currentFrame = (index) => `./project-scroll/${(index + 1).toString()}.jpg`;
+
+const images = [];
+let phone = { frame: 0 };
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  console.log(currentFrame(i));
+  images.push(img);
+}
+
+gsap.to(phone, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: "none",
+  scrollTrigger: {
+    scrub: 0.5,
+    pin: "canvas",
+    end: "80%",
+  },
+  onUpdate: render,
+});
+
+gsap.fromTo(
+  ".text-project-todolist",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    scrollTrigger: {
+      scrub: 1,
+      start: "50%",
+      end: "60%",
+    },
+    onComplete: () => {
+      gsap.to(".text-project-todolist", { opacity: 1 });
+    },
+  }
+);
+
+
+
+images[0].onload = render;
+
+function render() {
+  context.canvas.width = images[0].width;
+  context.canvas.height = images[0].height;
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[phone.frame], 0, 0);
+}
